84756e99485e58b20c1f04b952a18cf10dcc7c90
jcqnly <jcqnly@gmail.com>
2018-06-30 12:05:32 -0700

    add api end routes for TodoItems

--- ToDoApi/ToDoApi/Controllers/TodoListController.cs
+++ ToDoApi/ToDoApi/Controllers/TodoListController.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//this is the controller class for the todo LIST
 namespace ToDoApi.Controllers
 {
     [Route("api/[controller]")]
