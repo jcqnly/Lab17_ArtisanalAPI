6a7238f2c667faaed08208fc048bba4653c98ca3
jcqnly <jcqnly@gmail.com>
2018-06-30 09:33:36 -0700

    add scaffolding and api controller

---
+++ ToDoApi/ToDoApi/Model/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Model
+{
+    public class TodoList
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<TodoItem> ItemList { get; set; }
+    }
+}
