3e8d91b4bfadb50f3fd802d57d82e8bef5906c54
jcqnly <jcqnly@gmail.com>
2018-07-01 16:34:57 -0700

    deploy api to azure

--- ToDoApi/ToDoApi/Startup.cs
+++ ToDoApi/ToDoApi/Startup.cs
@@ -50,7 +50,7 @@ namespace ToDoApi
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello There!");
             });
 
         }
