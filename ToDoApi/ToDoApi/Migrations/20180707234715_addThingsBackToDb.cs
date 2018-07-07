89e5e5fedbe123f9520c2a34e092432e7cb6603c
jcqnly <jcqnly@gmail.com>
2018-07-07 16:47:39 -0700

    re populate db

---
+++ ToDoApi/ToDoApi/Migrations/20180707234715_addThingsBackToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoApi.Migrations
+{
+    public partial class addThingsBackToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
