2c3d9549ef1dc74d6c610adcf2f70880cdd6de7d
jcqnly <jcqnly@gmail.com>
2018-07-06 21:13:23 -0700

    add swagger connection routes

--- ToDoApi/ToDoApi/Startup.cs
+++ ToDoApi/ToDoApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using ToDoApi.Model;
 
 namespace ToDoApi
@@ -31,11 +32,23 @@ namespace ToDoApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<TodoContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //define docs to be added by Swaggel and connect to the Swashbuckle.AspNetCore nuget package
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ToDoAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {   //add endpoints for Swagger
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDoAPI V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
